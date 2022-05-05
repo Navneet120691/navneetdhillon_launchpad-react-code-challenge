6c362a2366542be64de67b9c622565c470340f88
navgit <navneetdhillon12691@gmail.com>
2022-05-05 17:02:59 -0600

    Fetch posts from end points

--- navneetdhillon_launchpad-react-code-challenge/src/pages/home/Home.js
+++ navneetdhillon_launchpad-react-code-challenge/src/pages/home/Home.js
@@ -1,7 +1,25 @@
+import { useState, useEffect} from 'react';
+import Axios from "axios";
+import axios from 'axios';
 const Home = () => {
-    return (
-        <h1>Home Page</h1>
-    );
-}
+const [posts, setPosts] = useState("");
+useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts? _start=0&_limit=20").then ((data) => {
+        setPosts(data.data)
+    });
+}, [posts]);
+return (
+    <div className="post-container">
+      {posts &&
+        posts.map((post) => (
+          <div key={post.id} className="post">
+               <div className="id">{post.id}</div>
+            <div className="title">{post.title}</div>
+            <div className="body">{post.body}</div>
+          </div>
+        ))}
+    </div>
+  );
+};
  
 export default Home;
