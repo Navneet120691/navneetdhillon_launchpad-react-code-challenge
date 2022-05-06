fde2cf4153ba61c5324c5fb06d67cc024f4ff4fa
navgit <navneetdhillon12691@gmail.com>
2022-05-06 17:20:16 -0600

    Delete post by ID

--- navneetdhillon_launchpad-react-code-challenge/src/pages/Postal lookup/PostalLookup.js
+++ navneetdhillon_launchpad-react-code-challenge/src/pages/Postal lookup/PostalLookup.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+
 const PostalLookup = () => {
-    return ( 
-        <h1>Postal lookup Page</h1>
-     );
-}
- 
+const [code, setCode] = useState("");
+const [area, setArea] = useState("");
+
+const handleSubmit = (e) => {
+e.preventDefault();
+axios.get(`https://api.zippopotam.us/us/${code} `).then((data) => {
+setArea(data.data);
+});
+};
+
+return (
+<div>
+<form onSubmit={handleSubmit}>
+<label>
+<input
+type="text"
+value={code}
+onChange={(e) => setCode(e.target.value)}
+/>
+</label>
+<button>Search</button>
+</form>
+{area && (
+<div className="area">
+<h2 className="country">{area.country}</h2>
+{area.places.map((place) => (
+<div key={place.latitude}>
+<div className="longitude">Longitude: {place.longitude}</div>
+<div className="latitude">Latitude: {place.latitude}</div>
+<div className="state">State: {place.state}</div>
+</div>
+))}
+</div>
+)}
+</div>
+);
+};
+
 export default PostalLookup;
--- navneetdhillon_launchpad-react-code-challenge/src/pages/home/Home.js
+++ navneetdhillon_launchpad-react-code-challenge/src/pages/home/Home.js
@@ -1,14 +1,45 @@
 import { useState, useEffect} from 'react';
-import Axios from "axios";
 import axios from 'axios';
+
 const Home = () => {
 const [posts, setPosts] = useState("");
+const [postId, setPostId] = useState("");
+
 useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts? _start=0&_limit=20").then ((data) => {
         setPosts(data.data)
     });
-}, [posts]);
+}, []);
+
+const handleSearch = (e) => {
+  e.preventDefault();
+  axios
+  .get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  .then((data) => setPosts([data.data]));
+  };
+ 
+  const handleDelete = (id) => {
+    axios
+    .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((data) => {
+    const newPosts = posts.filter((post) => post.id !== id);
+    setPosts(newPosts);
+    });
+    };
+
 return (
+  <>
+  <form onSubmit={handleSearch}>
+<label>
+<span>Search</span>
+<input
+type="text"
+value={postId}
+onChange={(e) => setPostId(e.target.value)}
+/>
+</label>
+<button>Search Post</button>
+</form>
     <div className="post-container">
       {posts &&
         posts.map((post) => (
@@ -16,9 +47,22 @@ return (
                <div className="id">{post.id}</div>
             <div className="title">{post.title}</div>
             <div className="body">{post.body}</div>
+            <div style={{ display: "flex", marginTop: "10px" }}>
+<button className="edit" style={{ marginRight: "2px" }}>
+Edit
+</button>
+<button
+className="delete"
+onClick={() => handleDelete(post.id)}
+>
+Delete
+</button>
+</div>
+
           </div>
         ))}
     </div>
+    </>
   );
 };
  
