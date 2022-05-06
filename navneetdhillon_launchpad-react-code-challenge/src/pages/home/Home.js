70dfe5527abf98a62ddb38db3613ae416069cb9b
navgit <navneetdhillon12691@gmail.com>
2022-05-06 17:02:08 -0600

    Search by ID

--- navneetdhillon_launchpad-react-code-challenge/src/pages/home/Home.js
+++ navneetdhillon_launchpad-react-code-challenge/src/pages/home/Home.js
@@ -2,12 +2,32 @@ import { useState, useEffect} from 'react';
 import axios from 'axios';
 const Home = () => {
 const [posts, setPosts] = useState("");
+const [postId, setPostId] = useState("");
 useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts? _start=0&_limit=20").then ((data) => {
         setPosts(data.data)
     });
-}, [posts]);
+}, []);
+const handleSearch = (e) => {
+  e.preventDefault();
+  axios
+  .get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  .then((data) => setPosts([data.data]));
+  };
+  
 return (
+  <>
+  <form onSubmit={handleSearch}>
+<label>
+<span>Search</span>
+<input
+type="text"
+value={postId}
+onChange={(e) => setPostId(e.target.value)}
+/>
+</label>
+<button>Search Post</button>
+</form>
     <div className="post-container">
       {posts &&
         posts.map((post) => (
@@ -18,6 +38,7 @@ return (
           </div>
         ))}
     </div>
+    </>
   );
 };
  
